658aa47918f0ec1b57e6a88febe836b3d30a199d
firstresort2526-eng <firstresort2526@gmail.com>
2026-03-30 23:27:12 +0800

    Added GPIO control

--- testing/1/GPIO_pin.py
+++ testing/1/GPIO_pin.py
@@ -6,7 +6,7 @@ state = True
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 while True:
-    respond = input("Open or close")
+    respond = input("Open or close?")
     state = True if respond=="open" else False
     GPIO_state = GPIO.HIGH if state else GPIO.LOW
     GPIO.output(PIN, GPIO_state)
